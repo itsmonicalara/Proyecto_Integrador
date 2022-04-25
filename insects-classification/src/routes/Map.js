6c82c30474474600553c8ecb3f99c8e8ce8bc68f
Aldo Ortiz <aortizm@live.com.mx>
2022-04-25 15:25:19 -0500

    Calendario
    Falta resolver erroes de conección con el servidor.

--- insects-classification/src/routes/Map.js
+++ insects-classification/src/routes/Map.js
@@ -1,21 +1,171 @@
-import React from 'react'
-import '../styles/Map.css'
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-import MapChart from "../MapChart";
+import React, {useState, useEffect} from 'react';
+import '../styles/Map.css';
+import format from 'date-fns/format';
+import getDay from 'date-fns/getDay';
+import parse from 'date-fns/parse';
+import startOfWeek from 'date-fns/startOfWeek';
+import {Calendar, dateFnsLocalizer} from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+//|import DatePicker from 'react-datepicker';
+//import 'react-datepicker/dist/react-datepicker.css';
+import axios from 'axios';
 
+const locales = {
+	'en-US': require('date-fns/locale/en-US'),
+};
 
-function Map() {    
+const localizer = dateFnsLocalizer({
+	format,
+	parse,
+	startOfWeek,
+	getDay,
+	locales,
+});
 
-  return (
-    <div className='map'>
-        <h1>Eventos</h1>
-        <h2>Todo lo que tienes que saber sobre las arañas!</h2>
-        <h3>3 de abril de 2022 - 18:00 pm</h3>
-        <div className='map_info'>
-          <MapChart />
-        </div>
-    </div>
-  )
+const events = [
+	{
+		title: 'Big Meeting',
+		allDay: true,
+		start: new Date(2022, 3, 29),
+		end: new Date(2022, 3, 29),
+	},
+	{
+		title: 'Vacation',
+		start: new Date(2022, 3, 7),
+		end: new Date(2022, 3, 10),
+	},
+	{
+		title: 'Conference',
+		start: new Date(2022, 3, 20),
+		end: new Date(2022, 3, 23),
+	},
+];
+
+function Map() {
+	const [newEvent, setNewEvent] = useState({
+		title: '',
+		start: '',
+		end: '',
+		events: [],
+	});
+	const [allEvents, setAllEvents] = useState(events);
+
+	function handleAddEvent() {
+		setAllEvents([...allEvents, newEvent]);
+	}
+
+	useEffect(() => {
+		getEvent();
+	});
+
+	const getEvent = () => {
+		axios
+			.get('http://localhost:4000/proyecto_db')
+			.then(response => {
+				const data = response.data;
+				this.setState({events: data});
+				console.log('Data has been received');
+			})
+			.catch(() => {
+				console.log('Error retreiving data');
+			});
+	};
+
+	const submit = event => {
+		event.preventDefault();
+
+		const payload = {
+			title: newEvent.title,
+			start: newEvent.start,
+			end: newEvent.end,
+		};
+
+		axios({
+			url: 'http://localhost:4000/proyecto_db',
+			method: 'POST',
+			data: payload,
+		})
+			.then(() => {
+				console.log('Data has been sent to the server');
+				resetUserInputs();
+			})
+			.catch(() => {
+				console.log('Internal server error');
+			});
+	};
+
+	const resetUserInputs = () => {
+		setNewEvent({
+			title: '',
+			start: '',
+			end: '',
+		});
+	};
+
+	const displayEvent = events => {
+		if (!events.length) return null;
+
+		return events.map((event, index) => (
+			<div key={index} className="event__display">
+				<h3>{event.title}</h3>
+				<h3>{event.start}</h3>
+				<h3>{event.end}</h3>
+			</div>
+		));
+	};
+
+	return (
+		<div className="map">
+			<h1>Calendario de Eventos</h1>
+			<h2>Agregar nuevo evento</h2>
+			<form onSubmit={submit}>
+				<div>
+					<input
+						type="text"
+						placeholder="Título"
+						style={{width: '20%', marginRight: '10px'}}
+						value={newEvent.title}
+						onChange={e => setNewEvent({...newEvent, title: e.target.value})}
+					/>
+					<input
+						type="text"
+						placeholder="Inicio"
+						style={{marginRight: '10px'}}
+						value={newEvent.start}
+						onChange={start => setNewEvent({...newEvent, start: start.target.value})}
+					/>
+					<input
+						type="text"
+						placeholder="Fin"
+						value={newEvent.end}
+						onChange={end => setNewEvent({...newEvent, end: end.target.value})}
+					/>
+					{/* <input
+					type="text"
+					placeholder="Descripción"
+					value={newEvent.description}
+					onChange={description => setNewEvent({...newEvent, end: description.target.value})}
+				/>
+				<input
+					type="text"
+					placeholder="url"
+					value={newEvent.url}
+					onChange={url => setNewEvent({...newEvent, end: url.target.value})}
+				/> */}
+					<button stlye={{marginTop: '10px'}} onClick={handleAddEvent}>
+						Add Event
+					</button>
+				</div>
+			</form>
+			<Calendar
+				localizer={localizer}
+				events={allEvents}
+				startAccessor="start"
+				endAccessor="end"
+				style={{height: 500, margin: '50px'}}
+			/>
+		</div>
+	);
 }
 
-export default Map
+export default Map;
--- server/server.js
+++ server/server.js
@@ -5,76 +5,105 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const spiderRoutes = express.Router();
+const eventRoutes = express.Router();
 
 const PORT = 4000;
 
+// La forma en que se llama el esquema
 let Spider = require('./spider.model');
+let Event = require('./event.model');
 
 app.use(cors());
 app.use(bodyParser.json());
 // Name of database: proyecto_db
-mongoose.connect('mongodb://127.0.0.1:27017/proyecto_db', { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/proyecto_db', {useNewUrlParser: true});
 const connection = mongoose.connection;
-connection.once('open', function() {
-    console.log("MongoDB database connection established successfully");
-})
+connection.once('open', function () {
+	console.log('MongoDB database connection established successfully');
+});
 
+// Hacer algo similar para calendario usar, probar con la misma ruta o crear mi propia ruta "add-calendar"
 // Add a spider to the database
-spiderRoutes.route('/add').post(function(req, res) {
-    let spider = new Spider(req.body);
-    spider.save()
-        .then(spider => {
-            res.status(200).json({'Status': 'Spider added successfully'});
-        })
-        .catch(err => {
-            res.status(400).send('Status: Adding new spider failed');
-        });
+spiderRoutes.route('/add').post(function (req, res) {
+	let spider = new Spider(req.body);
+	spider
+		.save()
+		.then(spider => {
+			res.status(200).json({Status: 'Spider added successfully'});
+		})
+		.catch(err => {
+			res.status(400).send('Status: Adding new spider failed');
+		});
+});
+
+eventRoutes.route('/add-event').post(function (req, res) {
+	let event = new Event(req.body);
+	event
+		.save()
+		.then(event => {
+			res.status(200).json({Status: 'Event added successfully'});
+		})
+		.catch(err => {
+			res.status(400).send('Status: Adding new event failed');
+		});
 });
 
 // Retrieve all the spiders in the database
-spiderRoutes.route('/').get(function(req, res) {
-    Spider.find(function(err, proyecto_db) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.json(proyecto_db);
-        }
-    });
+spiderRoutes.route('/').get(function (req, res) {
+	Spider.find(function (err, proyecto_db) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.json(proyecto_db);
+		}
+	});
 });
 
+// Retrieve all the events in the database
+// eventRoutes.route('/get-events').get(function (req, res) {
+// 	Event.find(function (err, proyecto_db) {
+// 		if (err) {
+// 			console.log(err);
+// 		} else {
+// 			res.json(proyecto_db);
+// 		}
+// 	});
+// });
+
 // Get a single spider by id
-spiderRoutes.route('/:id').get(function(req, res) {
-    let id = req.params.id;
-    Spider.findById(id, function(err, spider) {
-        res.json(spider);
-    });
+spiderRoutes.route('/:id').get(function (req, res) {
+	let id = req.params.id;
+	Spider.findById(id, function (err, spider) {
+		res.json(spider);
+	});
 });
 
 // Update a spider by id
-spiderRoutes.route('/update/:id').post(function(req, res) {
-    Spider.findById(req.params.id, function(err, spider) {
-        if (!spider)
-            res.status(404).send("data is not found");
-        else
-            spider.name = req.body.name;
-            spider.description = req.body.description;
-            spider.danger = req.body.danger;
-            spider.url = req.body.url;
-            spider.info.species = req.body.info.species;
-            spider.info.family = req.body.info.family;
-            spider.info.filo = req.body.info.filo;
-            spider.info.scientificName = req.body.info.scientificName;
-            spider.save().then(spider => {
-                res.json('Spider updated!');
-            })
-            .catch(err => {
-                res.status(400).send("Update not possible");
-            });
-    });
+spiderRoutes.route('/update/:id').post(function (req, res) {
+	Spider.findById(req.params.id, function (err, spider) {
+		if (!spider) res.status(404).send('data is not found');
+		else spider.name = req.body.name;
+		spider.description = req.body.description;
+		spider.danger = req.body.danger;
+		spider.url = req.body.url;
+		spider.info.species = req.body.info.species;
+		spider.info.family = req.body.info.family;
+		spider.info.filo = req.body.info.filo;
+		spider.info.scientificName = req.body.info.scientificName;
+		spider
+			.save()
+			.then(spider => {
+				res.json('Spider updated!');
+			})
+			.catch(err => {
+				res.status(400).send('Update not possible');
+			});
+	});
 });
 
 app.use('/proyecto_db', spiderRoutes);
+app.use('/proyecto_db', eventRoutes);
 
-app.listen(PORT, function() {
-    console.log("Server is running on Port: " + PORT);
+app.listen(PORT, function () {
+	console.log('Server is running on Port: ' + PORT);
 });
