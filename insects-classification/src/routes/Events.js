27321530df58d92ddeaac862c7e081020569b3a9
Monica Lara <38739285+itsmonicalara@users.noreply.github.com>
2022-04-29 04:03:42 -0500

    Modified profile and change the events layout

--- insects-classification/src/routes/Events.js
+++ insects-classification/src/routes/Events.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import '../styles/Events.css'
+import { Text } from '@nextui-org/react';
 
 
 function Events() {    
 
   return (
     <div className='events'>
-        <h1>Eventos</h1>
-        
+      <div className='events-title'>
+        <Text
+          h1
+          size={60}
+          css={{
+            textGradient: "45deg, $yellow500 -20%, $red500 100%",
+          }}
+          weight="bold"
+        >
+          Próximos eventos
+        </Text>   
+      </div>
+         
     </div>
   )
 }
--- insects-classification/src/routes/Profile.js
+++ insects-classification/src/routes/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
-import Avatar from '@material-ui/core/Avatar';
+import { Text, Avatar } from '@nextui-org/react';
 import '../styles/Profile.css'
 
      
@@ -12,7 +12,21 @@ function Profile() {
   return (
     isAuthenticated && (
         <div className='profile_info'>
-            <Avatar src={user.picture} alt='Profile'></Avatar>
+            <Text
+            h1
+            size={60}
+            css={{
+              textGradient: "45deg, $blue500 -20%, $pink500 50%",
+              marginTop: "3%",
+              marginLeft: "3%",
+            }}
+            weight="bold"
+            >
+            Perfil
+          </Text>
+          <br></br>
+            <Avatar squared src={user.picture} alt='Profile' zoomed css={{ size: "$20", marginLeft: "3%", marginTop: "3%" }}></Avatar>
+            <br></br>
             <h2>{user.name}</h2>
             <p>{user.email}</p>
             <p>{user.email_verified}</p>
